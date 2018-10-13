dcf63364d0901a69c1608372febfa28e23113947
ashleyco <ashley.colley@gmail.com>
2018-10-13 14:45:09 +0300

    Bug fixes

--- Move.cs
+++ Move.cs
@@ -18,14 +18,14 @@ using System.IO.Ports;
 public class Move : MonoBehaviour {
 	// *** NOTE YOU HAVE TO SET THE RIGHT SERIAL PORT EACH TIME ***
 	//SerialPort sp = new SerialPort("COM3", 9600); // windows example
-	SerialPort sp = new SerialPort("/dev/cu.usbserial-14110", 115200);  // Mac example
+	SerialPort sp = new SerialPort("/dev/cu.usbserial-14310", 115200);  // Mac example
 	string inputString = "";
 	char[] inputChars;
 	GameObject layer0, layer1, layer2, layer3;
 	Color color0, color1, color2, color3;
 	Color curColor; // used in the fading
 	float alphaNow, alphaDiff; // used in fading
-	float fadespeed = 0.5f;
+
 	float fixedStep = 0.05f;
 	float closeEnough = 0.1f;
 
@@ -44,68 +44,66 @@ public class Move : MonoBehaviour {
 		try{
 			inputString= sp.ReadLine();
 			// for each sensor
-			char input0  = inputString[0];
-			float targetOpacity0 =  float.Parse(""+input0);
-			curColor = layer0.GetComponent<Renderer>().material.color;
-			if (curColor.a > targetOpacity0 - closeEnough)
-			{
-				curColor.a = curColor.a - fixedStep;
-				layer0.GetComponent<Renderer>().material.SetColor("_Color", curColor);
+				print(inputString);
 			}
-			if (curColor.a < targetOpacity0 + closeEnough)
-			{
-				curColor.a = curColor.a + fixedStep;
+				catch(System.Exception){
+			}
+		if (inputString.Length == 4 ) {
 
+			char input0 = inputString [0];
+			float targetOpacity0 = float.Parse ("" + input0);
+			curColor = layer0.GetComponent<Renderer> ().material.color;
+			//print (curColor);
+			if (curColor.a > targetOpacity0 - closeEnough) {
+				curColor.a = curColor.a - fixedStep;
+				layer0.GetComponent<Renderer> ().material.SetColor ("_Color", curColor);
+			}
+			if (curColor.a < targetOpacity0 + closeEnough) {
+				curColor.a = curColor.a + fixedStep;
+				layer0.GetComponent<Renderer> ().material.SetColor ("_Color", curColor);
 			}
 				
-			char input1  = inputString[1];
-			//print (input0);
-			float targetOpacity1 =  float.Parse(""+input1);
-			curColor = layer1.GetComponent<Renderer>().material.color;
-			if (curColor.a > targetOpacity1 - closeEnough)
-			{
+			char input1 = inputString [1];
+			//print (input1);
+			float targetOpacity1 = float.Parse ("" + input1);
+			curColor = layer1.GetComponent<Renderer> ().material.color;
+			if (curColor.a > targetOpacity1 - closeEnough) {
 				curColor.a = curColor.a - fixedStep;
-				layer0.GetComponent<Renderer>().material.SetColor("_Color", curColor);
+				layer1.GetComponent<Renderer> ().material.SetColor ("_Color", curColor);
 			}
-			if (curColor.a < targetOpacity1 +closeEnough)
-			{
+			if (curColor.a < targetOpacity1 + closeEnough) {
 				curColor.a = curColor.a + fixedStep;
-				layer0.GetComponent<Renderer>().material.SetColor("_Color", curColor);
+				layer1.GetComponent<Renderer> ().material.SetColor ("_Color", curColor);
 			}
 				
-			char input2  = inputString[2];
-			//print (input0);
-			float targetOpacity2 =  float.Parse(""+input2);
-			curColor = layer2.GetComponent<Renderer>().material.color;
-			if (curColor.a > targetOpacity2 - closeEnough)
-			{
+			char input2 = inputString [2];
+			//print (input2);
+			float targetOpacity2 = float.Parse ("" + input2);
+			curColor = layer2.GetComponent<Renderer> ().material.color;
+			if (curColor.a > targetOpacity2 - closeEnough) {
 				curColor.a = curColor.a - fixedStep;
-				layer0.GetComponent<Renderer>().material.SetColor("_Color", curColor);
+				layer2.GetComponent<Renderer> ().material.SetColor ("_Color", curColor);
 			}
-			if (curColor.a < targetOpacity2 + closeEnough)
-			{
+			if (curColor.a < targetOpacity2 + closeEnough) {
 				curColor.a = curColor.a + fixedStep;
-				layer0.GetComponent<Renderer>().material.SetColor("_Color", curColor);
+				layer2.GetComponent<Renderer> ().material.SetColor ("_Color", curColor);
 			}
 
-			char input3  = inputString[3];
-			print (input3);
-			float targetOpacity3 =  float.Parse(""+input3);
-			curColor = layer3.GetComponent<Renderer>().material.color;
-			if (curColor.a > targetOpacity3 - closeEnough)
-			{
+			char input3 = inputString [3];
+			//print (input3);
+			float targetOpacity3 = float.Parse ("" + input3);
+			curColor = layer3.GetComponent<Renderer> ().material.color;
+			if (curColor.a > targetOpacity3 - closeEnough) {
 				curColor.a = curColor.a - fixedStep;
-				layer0.GetComponent<Renderer>().material.SetColor("_Color", curColor);
+				layer3.GetComponent<Renderer> ().material.SetColor ("_Color", curColor);
 			}
-			if (curColor.a < targetOpacity3 + closeEnough)
-			{
+			if (curColor.a < targetOpacity3 + closeEnough) {
 				curColor.a = curColor.a + fixedStep;
-				layer0.GetComponent<Renderer>().material.SetColor("_Color", curColor);
+				layer3.GetComponent<Renderer> ().material.SetColor ("_Color", curColor);
 			}
 				
-		}
-		catch(System.Exception){
-		}
+		}// end if
+
 	}
 
 }
